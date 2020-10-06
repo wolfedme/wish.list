df5c4e7cc675faf312a75b91ba23c6549785bd93
Maximilian Wolf <max@wolfed.me>
2020-10-06 20:28:52 +0200

    added firebase config & packages

---
+++ src/globals/firebase/config.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+  export default firebaseConfig;
+
+  //TODO: Paste your Firebase Config
