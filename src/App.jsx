bea1f0192b00bfb678e20360644dc56b5fd7376a
Maximilian Wolf <max@wolfed.me>
2020-10-06 20:55:37 +0200

    Renamed to .jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import 'firebase/firestore';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">/* TODO */</header>
+    </div>
+  );
+}
+
+export default App;
+
+// TODO: Write tests!
