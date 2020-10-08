e4af7b3b7588d51f04b59a46274ef36d3a55a24f
Maximilian Wolf <max@wolfed.me>
2020-10-08 14:29:15 +0200

    Implemented firebase context

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Firebase, { FirebaseContext } from './globals/firebase';
+
 import './globals/i18n/i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
