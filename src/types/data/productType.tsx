dfaf384d3135e33000ab2efc7f52d587938fc327
Maximilian Wolf <max@wolfed.me>
2021-03-23 22:20:36 +0100

    added more properties to product

--- src/types/data/productType.tsx
+++ src/types/data/productType.tsx
@@ -2,11 +2,15 @@ export interface Product {
   id: number;
   name: string;
   isReserved: boolean;
+  reservedBy?: string;
   description?: string;
   price?: number;
+  currency?: Currency;
   vendor?: string;
   imgUrl?: string;
   link?: string;
+  tags?: string[];
+  category?: string;
 }
 
 export interface Currency {
